4c61a00c328142f9ce449e1846f951ddc0e5bd11
Neil Rizen <33267501+newrizen@users.noreply.github.com>
2024-11-20 08:10:25 -0300

    Update game.js

--- game.js
+++ game.js
@@ -247,7 +247,7 @@ export class SnakeGame {
       this.ctx.fillText(
         CONFIG.DEFAULT_EMOJI2,
         this.rightPaddle.x - 5, // Align emoji properly
-        this.rightPaddle.y + (this.ballSize / spacingFactor + 7) * (i + 2) // spacingFactor
+        this.rightPaddle.y + (this.ballSize + 10) * (i) // spacingFactor
       );
     }
 
