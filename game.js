d2bebf75e5b4665063a4d09034be491cc9cebcd0
Neil Rizen <33267501+newrizen@users.noreply.github.com>
2024-11-21 20:55:08 -0300

    Update game.js

--- game.js
+++ game.js
@@ -258,28 +258,28 @@ update() {
     if (this.leftPaddle.electrified) {
       this.ctx.fillText(
         CONFIG.ELECTRIFIED_EMOJI,
-        this.leftPaddle.x, //+ this.paddleWidth / 2, // Centralizado no paddle
-        this.leftPaddle.y // Na borda superior do paddle
+        this.leftPaddle.x + this.paddleWidth / 2, // Centralizado no paddle
+        this.leftPaddle.y  // Na borda superior do paddle
       );
-      this.ctx.fillText(
-        CONFIG.ELECTRIFIED_EMOJI,
-        this.leftPaddle.x, //+ this.paddleWidth / 2,
-        this.leftPaddle.y + this.paddleHeight // Na borda inferior do paddle
-      );
+      //this.ctx.fillText(
+      //  CONFIG.ELECTRIFIED_EMOJI,
+      //  this.leftPaddle.x + this.paddleWidth / 2,
+      //  this.leftPaddle.y + this.paddleHeight // Na borda inferior do paddle
+      //);
     }
     
     // Desenha o paddle direito com electrified
     if (this.rightPaddle.electrified) {
       this.ctx.fillText(
         CONFIG.ELECTRIFIED_EMOJI,
-        this.rightPaddle.x, //- this.paddleWidth / 2, // Centralizado no paddle
+        this.rightPaddle.x - this.paddleWidth / 2, // Centralizado no paddle
         this.rightPaddle.y // Na borda superior do paddle
       );
-      this.ctx.fillText(
-        CONFIG.ELECTRIFIED_EMOJI,
-        this.rightPaddle.x, //- this.paddleWidth / 2,
-        this.rightPaddle.y + this.paddleHeight // Na borda inferior do paddle
-      );
+      //this.ctx.fillText(
+      //  CONFIG.ELECTRIFIED_EMOJI,
+      //  this.rightPaddle.x, //- this.paddleWidth / 2,
+      //  this.rightPaddle.y + this.paddleHeight // Na borda inferior do paddle
+      //);
     }
 
     // Draw ball as BOMB_EMOJI
