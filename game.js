6bb33b28d3de19589150cde0a548b6a1cfc8eb11
Neil Rizen <33267501+newrizen@users.noreply.github.com>
2024-11-22 18:33:03 -0300

    Update game.js

--- game.js
+++ game.js
@@ -245,13 +245,13 @@ update() {
     if (this.leftPaddle.electrified) {
       this.ctx.fillText(
         leftElectrifiedEmoji,
-        this.leftPaddle.x + this.paddleWidth, // Centralizado no paddle
+        this.leftPaddle.x + this.paddleWidth - 5, // Centralizado no paddle
         this.leftPaddle.y - 5 // Na borda superior do paddle
       );
       this.ctx.fillText(
         leftElectrifiedEmoji,
-        this.leftPaddle.x + this.paddleWidth,
-        this.leftPaddle.y + this.paddleHeight + this.peddleCellSize // Na borda inferior do paddle
+        this.leftPaddle.x + this.paddleWidth - 5,
+        this.leftPaddle.y + this.paddleHeight // Na borda inferior do paddle
       );
     }
     
@@ -274,7 +274,7 @@ update() {
         this.ctx.fillText(
             rightEyeEmoji,
             this.rightPaddle.x - 5,
-            this.rightPaddle.y + this.peddleCellSize * 2 // Início do paddle
+            this.rightPaddle.y + this.peddleCellSize // Início do paddle
         );
     }
     
@@ -282,12 +282,12 @@ update() {
     if (this.rightPaddle.electrified) {
       this.ctx.fillText(
         rightElectrifiedEmoji,
-        this.rightPaddle.x - this.paddleWidth, // Centralizado no paddle
+        this.rightPaddle.x - this.paddleWidth - 5, // Centralizado no paddle
         this.rightPaddle.y - 5 // Na borda superior do paddle
       );
       this.ctx.fillText(
         rightElectrifiedEmoji,
-        this.rightPaddle.x - this.paddleWidth,
+        this.rightPaddle.x - this.paddleWidth - 5,
         this.rightPaddle.y + this.paddleHeight // Na borda inferior do paddle
       );
     }
