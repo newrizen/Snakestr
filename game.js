6d7a8f17c9389646c5efd1cc82540aef6f764dca
Neil Rizen <33267501+newrizen@users.noreply.github.com>
2024-11-22 19:03:02 -0300

    Update game.js

--- game.js
+++ game.js
@@ -246,12 +246,12 @@ update() {
       this.ctx.fillText(
         leftElectrifiedEmoji,
         this.leftPaddle.x + this.paddleWidth, // Centralizado no paddle
-        this.leftPaddle.y - 5 // Na borda superior do paddle
+        this.leftPaddle.y + 5 // Na borda superior do paddle
       );
       this.ctx.fillText(
         leftElectrifiedEmoji,
         this.leftPaddle.x + this.paddleWidth,
-        this.leftPaddle.y + this.paddleHeight - 10 // Na borda inferior do paddle
+        this.leftPaddle.y + this.paddleHeight // Na borda inferior do paddle
       );
     }
     
@@ -283,12 +283,12 @@ update() {
       this.ctx.fillText(
         rightElectrifiedEmoji,
         this.rightPaddle.x - this.paddleWidth - 5, // Centralizado no paddle
-        this.rightPaddle.y - 5 // Na borda superior do paddle
+        this.rightPaddle.y + 5 // Na borda superior do paddle
       );
       this.ctx.fillText(
         rightElectrifiedEmoji,
         this.rightPaddle.x - this.paddleWidth - 5,
-        this.rightPaddle.y + this.paddleHeight - 10 // Na borda inferior do paddle
+        this.rightPaddle.y + this.paddleHeight // Na borda inferior do paddle
       );
     }
 
