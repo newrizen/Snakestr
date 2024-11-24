e234299693d70d17b8cd3493606b4927b5d72e5a
Neil Rizen <33267501+newrizen@users.noreply.github.com>
2024-11-24 20:06:31 -0300

    Update game.js

--- game.js
+++ game.js
@@ -134,7 +134,7 @@ export class SnakeGame {
 
 addRockEmoji() {
     // Gera uma nova pedra em intervalos aleatórios
-    if (Math.random() < 0.01) { // Ajuste a frequência de geração conforme necessário
+    if (Math.random() < 0.1) { // 0.01 Ajuste a frequência de geração conforme necessário
         const randomX = Math.random() * (this.canvas.width * 2/3 - this.rockBlockSize) + this.canvas.width/6;
         const randomY = Math.random() * (this.canvas.height - this.rockBlockSize);
 
