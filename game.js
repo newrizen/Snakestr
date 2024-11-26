d3c66d4963125eef232cd0f6c54c6a2321be0cea
Neil Rizen <33267501+newrizen@users.noreply.github.com>
2024-11-26 16:17:05 -0300

    Update game.js

--- game.js
+++ game.js
@@ -138,7 +138,7 @@ export class SnakeGame {
 update() {
     // Insere os emojis de rock de forma aleatória
     // Gera uma nova pedra em intervalos aleatórios
-    if (Math.random() < 0.03) { // 0.01 Ajuste a frequência de geração conforme necessário
+    if (Math.random() < 0.02) { // 0.01 Ajuste a frequência de geração conforme necessário
         const randomX = Math.random() * (this.canvas.width * 1/2 - this.rockBlockSize) + this.canvas.width/4;
         const randomY = Math.random() * (this.canvas.height - this.rockBlockSize);
 
@@ -184,7 +184,7 @@ update() {
         else if (collidedX) {
           // Ação quando a bola atinge a pedra (opcional)
           this.ball.dy *= -1;
-          this.ball.dy *= -1;
+          this.ball.dx *= -1;
           return false;  // Remove a pedra se colidida
         }
         else if (collidedY) {
