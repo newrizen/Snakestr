972968ca883a63e0429cec4def197aea2ef7eea0
Neil Rizen <33267501+newrizen@users.noreply.github.com>
2024-11-26 10:32:34 -0300

    Update game.js

--- game.js
+++ game.js
@@ -158,19 +158,19 @@ update() {
     this.rocks = this.rocks.filter(rock => {
       
         const collidedX = 
-          (this.ball.x <= rock.x + rock.size && 
-          this.ball.x + this.ballSize => rock.x && 
-          this.ball.y + this.ballSize => rock.y &&
-          this.ball.y <= rock.y + rock.size); //|| 
+          (this.ball.x >= rock.x + rock.size && 
+          this.ball.x + this.ballSize <= rock.x && 
+          this.ball.y + this.ballSize <= rock.y &&
+          this.ball.y >= rock.y + rock.size); //|| 
           //(this.ball.x <= rock.x + rock.size && 
           //this.ball.x + this.ballSize => rock.x && 
           //this.ball.y <= rock.y + rock.size);
 
         const collidedY =
-          (this.ball.y < rock.y + rock.size && 
-          this.ball.y + this.ballSize > rock.y && 
-          this.ball.x < rock.x + rock.size &&
-          this.ball.x + this.ballSize > rock.x); //||
+          (this.ball.y >= rock.y + rock.size && 
+          this.ball.y + this.ballSize <= rock.y && 
+          this.ball.x >= rock.x + rock.size &&
+          this.ball.x + this.ballSize <= rock.x); //||
           //(this.ball.y < rock.y + rock.size && 
           //this.ball.y + this.ballSize > rock.y && 
           //this.ball.x + this.ballSize > rock.x);
