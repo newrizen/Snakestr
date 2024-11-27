abf5e39d3c282e01e42bf98215a2cda18bda057f
Neil Rizen <33267501+newrizen@users.noreply.github.com>
2024-11-27 11:03:49 -0300

    Update game.js

--- game.js
+++ game.js
@@ -285,7 +285,7 @@ update() {
         this.ball.dy *= -1;
         // Ativa o electrified no paddle direito
         this.rightPaddle.electrified = true;
-        setTimeout(() => this.rightPaddle.electrified = false, 2000); // Dura 2 segundos
+        setTimeout(() => this.rightPaddle.electrified = false, 500); // Dura 2 segundos
     }
       
     else if (
@@ -297,7 +297,7 @@ update() {
         this.ball.dx *= -1;
         // Ativa o electrified no paddle direito
         this.rightPaddle.electrified = true;
-        setTimeout(() => this.rightPaddle.electrified = false, 2000); // Dura 2 segundos
+        setTimeout(() => this.rightPaddle.electrified = false, 500); // Dura 2 segundos
     }
 }
   
@@ -373,28 +373,6 @@ update() {
     //this.ctx.translate(0, this.leftPaddle.y + this.paddleWidth / 2); // Centralizar no paddle
     //this.ctx.scale(1, -1); // Espelhar verticalmente
     //this.ctx.translate(0, -(this.leftPaddle.y + this.paddleWidth / 2)); // Reverter a centralização
-
-    if (leftEmoji) {
-      this.ctx.save(); // Salvar estado inicial do contexto
-  
-      this.ctx.translate(this.leftPaddle.x + this.paddleWidth / 2, 0); // Move the reference point
-      this.ctx.scale(-1, 1); // Flip horizontally
-      this.ctx.translate(-(this.leftPaddle.x + this.paddleWidth / 2), 0); // Restore position
-
-      
-      this.ctx.restore(); // Restaurar o estado original do contexto
-    }
-
-    if (leftEyeEmoji) {
-      this.ctx.save(); // Salvar estado inicial do contexto
-  
-      this.ctx.translate(this.leftPaddle.x + this.eyeSize / 2, 0); // Move the reference point
-      this.ctx.scale(-1, 1); // Flip horizontally
-      this.ctx.translate(-(this.leftPaddle.x + this.eyeSize / 2), 0); // Restore position
-
-      
-      this.ctx.restore(); // Restaurar o estado original do contexto
-    }
     
     // Draw left paddle as DEFAULT_EMOJI1
     for (let i = 0; i < this.paddleHeight / (this.peddleCellSize + 2) - 1; i++) { // 100/ (600/60 * 2)
@@ -420,6 +398,30 @@ update() {
             this.leftPaddle.y + this.peddleCellSize // Início do paddle
         );
     }
+
+    
+    if (leftEmoji) {
+      this.ctx.save(); // Salvar estado inicial do contexto
+  
+      this.ctx.translate(this.leftPaddle.x + this.paddleWidth / 2, 0); // Move the reference point
+      this.ctx.scale(-1, 1); // Flip horizontally
+      this.ctx.translate(-(this.leftPaddle.x + this.paddleWidth / 2), 0); // Restore position
+
+      
+      this.ctx.restore(); // Restaurar o estado original do contexto
+    }
+
+    if (leftEyeEmoji) {
+      this.ctx.save(); // Salvar estado inicial do contexto
+  
+      this.ctx.translate(this.leftPaddle.x + this.eyeSize / 2, 0); // Move the reference point
+      this.ctx.scale(-1, 1); // Flip horizontally
+      this.ctx.translate(-(this.leftPaddle.x + this.eyeSize / 2), 0); // Restore position
+
+      
+      this.ctx.restore(); // Restaurar o estado original do contexto
+    }
+    
     // Desenha o paddle esquerdo com electrified
     if (this.leftPaddle.electrified) {
       this.ctx.fillText(
